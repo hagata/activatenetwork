86630525bb507e20d69740eb8dd23f7ebbfe4b5f
Jessie Kim <jekim@hugeinc.com>
2017-01-14 10:08:12 -0800

    Updated user data

--- activate/app/data/users.js
+++ activate/app/data/users.js
@@ -5,27 +5,27 @@ const Users = {
         'contacts': [
           {
           "message": "Hey, it’s Natalia. I’ve been detained by ICE and won’t make it home tonight.  Can you please pick Junior up from school? He usually gets out at 3:30, but on Fridays he has soccer practice. If you can't make it, please contact Genki or Cid. There’s emergency information in an envelope on the fridge. Thank you! 🙏🏽",
-          "name": "genki",
+          "name": "Cid Donham",
           "phoneNumber": "+19253254480"
           },
           {
           "message": "Hey, it’s Natalia. I’ve been detained by ICE and won’t make it home tonight.  Can you please pick Junior up from school? He usually gets out at 3:30, but on Fridays he has soccer practice. If you can't make it, please contact Genki or Cid. There’s emergency information in an envelope on the fridge. Thank you! 🙏🏽",
-          "name": "Jessie Kim",
+          "name": "Genki Hagata",
           "phoneNumber": "+13238545777"
           },
           {
           "message": "Hey, it’s Natalia. I’ve been detained by ICE and won’t make it home tonight.  Can you please pick Junior up from school? He usually gets out at 3:30, but on Fridays he has soccer practice. If you can't make it, please contact Genki or Cid. There’s emergency information in an envelope on the fridge. Thank you! 🙏🏽",
-          "name": "Talia Steinberg",
+          "name": "Gela Fridman",
           "phoneNumber": "+15104499800"
           },
           {
           "message": "Hey, it’s Natalia. I’ve been detained by ICE and won’t make it home tonight.  Can you please pick Junior up from school? He usually gets out at 3:30, but on Fridays he has soccer practice. If you can't make it, please contact Genki or Cid. There’s emergency information in an envelope on the fridge. Thank you! 🙏🏽",
-          "name": "cid",
+          "name": "Jessie Kim",
           "phoneNumber": "+13233855336"
           },
           {
           "message": "Hey, it’s Natalia. I’ve been detained by ICE and won’t make it home tonight.  Can you please pick Junior up from school? He usually gets out at 3:30, but on Fridays he has soccer practice. If you can't make it, please contact Genki or Cid. There’s emergency information in an envelope on the fridge. Thank you! 🙏🏽",
-          "name": "Gela",
+          "name": "Talia Steinberg",
           "phoneNumber": "+19176128298"
           }
         ]
