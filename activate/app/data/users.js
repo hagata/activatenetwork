a4daafb08e24a60423d69a6a9adf2452c6101efc
Cid Donham <cdonham@hugeinc.com>
2017-01-13 16:53:49 -0800

    adds labels to form

---
+++ activate/app/data/users.js
@@ -0,0 +1,29 @@
+const Users = {
+    '15104499800': {
+        'name': 'Natalia',
+        'pin': '1234',
+        'contacts': [
+            {
+                name: "Genki Hagata",
+                phoneNumber: "+19253254480",
+                message: "🚨 Your friend Natalia is in trouble. Your friend Natalia is in trouble. help!  🚓"
+            },
+            {
+                name: "Jessie Kim",
+                phoneNumber: "+13238545777",
+                message: "🚨 Your friend Natalia is in trouble. Hey Jessie, it's Natalia, please take care of my fish  🚓"
+            },
+            {
+                name: 'Talia Steinberg',
+                phoneNumber: "+15104499800",
+                message: "🚨 Your friend is in trouble. help!  🚓"
+            },
+            {
+                name: 'Cid Donham',
+                phoneNumber: "+3233855336",
+                message: "🚨 So much help needed!  🚓"
+            }
+        ]
+    }
+}
+export default Users;
